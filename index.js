46ae70a8e5900e5e78160224cd361f3267c2b32a
Arun <arun@adidas.com>
2022-09-27 10:59:41 +0530

    test commit

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+<<<<<<< Updated upstream
 var app = require('./server/newServer');
+=======
+var app = require('./server/srvr');
+var config = require('./server/config/config');
+>>>>>>> Stashed changes
 
 var config = require('./server/config/myConfig');
 
